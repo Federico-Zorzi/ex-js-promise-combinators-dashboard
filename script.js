3e0239a22395a21d1b5fc0d5e947cf6feb09e603
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-26 14:55:01 +0100

    new phrase with value null in object

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 // # EXERCISE ex-js-promise-combinators-dashboard
 
 async function getDashboardData(city) {
-  const responseDestination = fetch(
-    `https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`
-  )
-    .then((res) => res.json())
-    .catch(() => null);
+  try {
+    const responseDestination = fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${city}`
+    ).then((res) => res.json());
 
-  const responseWeather = fetch(
-    `https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`
-  )
-    .then((res) => res.json())
-    .catch(() => null);
+    const responseWeather = fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${city}`
+    ).then((res) => res.json());
 
-  const responseMainAirport = fetch(
-    `https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`
-  )
-    .then((res) => res.json())
-    .catch(() => null);
+    const responseMainAirport = fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${city}`
+    ).then((res) => res.json());
 
-  const [destination, weather, mainAirport] = await Promise.all([
-    responseDestination,
-    responseWeather,
-    responseMainAirport,
-  ]);
-  /* console.log("destination", destination);
+    const [destination, weather, mainAirport] = await Promise.all([
+      responseDestination,
+      responseWeather,
+      responseMainAirport,
+    ]);
+    /* console.log("destination", destination);
     console.log("weather", weather);
     console.log("mainAirport", mainAirport); */
 
-  return {
-    city: destination[0].name,
-    country: destination[0].country,
-    temperature: weather[0].temperature,
-    weather: weather[0].weather_description,
-    airport: mainAirport[0].name,
-  };
+    return {
+      city: destination[0]?.name ?? null,
+      country: destination[0]?.country ?? null,
+      temperature: weather[0]?.temperature ?? null,
+      weather: weather[0]?.weather_description ?? null,
+      airport: mainAirport[0]?.name ?? null,
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
 
-getDashboardData("london")
+getDashboardData("vienna")
   .then((data) => {
     console.log("Dasboard data:", data);
-    console.log(
-      `${data.city} is in ${data.country}.\n` +
-        `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
-        `The main airport is ${data.airport}.\n`
-    );
+    let finalPhrase = "";
+    if (data.city && data.country) {
+      finalPhrase += `${data.city} is in ${data.country}.\n`;
+    }
+    if (data.weather && data.temperature) {
+      finalPhrase += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
+    }
+    if (data.airport) {
+      finalPhrase += `The main airport is ${data.airport}.\n`;
+    }
+    finalPhrase.length > 0 ? console.log(finalPhrase) : null;
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.error(error))
+  .finally(() => console.log("Fine codice!"));
 
 /* 
    Risposta API
